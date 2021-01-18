3c608a908aae6cfa3f98043c407db01abfc328ae
Roman.Yurlov <yurlovr@gmail.com>
2021-01-18 14:52:29 +0500

    fix

--- server.js
+++ server.js
@@ -4,17 +4,7 @@ const connect = require('connect');
 
 const port = process.env.PORT || 8080;
 
-const app = connect()
+connect()
   .use(history())
   .use(express.static('dist'))
   .listen(port);
-
-// const app = express();
-
-// app.use(express.static('dist'));
-
-// const port = process.env.PORT || 8080;
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}!`);
-// });
