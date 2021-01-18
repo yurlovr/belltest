1b6992e925869209c701baa1d579cb87137df8e6
Roman.Yurlov <yurlovr@gmail.com>
2021-01-18 14:44:05 +0500

    fix

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
-
-const app = express();
-
-app.use(express.static('dist'));
+const history = require('connect-history-api-fallback');
+const connect = require('connect');
 
 const port = process.env.PORT || 8080;
+
+const app = connect()
+  .use(history())
+  .use(express.static('dist'))
+  .listen(3000);
+
+// const app = express();
+
+// app.use(express.static('dist'));
+
+// const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
